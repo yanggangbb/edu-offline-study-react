a2e200d66438cf5cbf95cc381157fcb05777037e
GyeongtaeGim <elnotlu@gmail.com>
2023-12-21 13:40:25 +0900

    🔧 vite.d.ts 파일명 변경 및 interface export

--- vite.d.ts
+++ vite.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
+export interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   // 다른 환경 변수들에 대한 타입 정의...
 }
 
-interface ImportMeta {
+export interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
