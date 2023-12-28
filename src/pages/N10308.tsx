8f088922dc300c38eefd8dbb0e64deaf286c114e
unknown <yjs12180825@gmail.com>
2023-12-28 20:15:01 +0900

    :sparkles: 학번 페이지를 윤상지가 리뉴얼했어용용

--- src/pages/N10308.tsx
+++ src/pages/N10308.tsx
@@ -1,9 +1,19 @@
 import { Stack, Typography } from "@mui/material"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const N10308 = () => {
+  const [color, setColor] = useState<string>("black");
+  const colors = ["black", "aqua", "red", "purple", "blue"]
+
+  useEffect(() => {
+    setInterval(() => {
+      const selectedColor = colors[Math.floor(Math.random() * colors.length)];
+      setColor(selectedColor);
+    }, 500)
+  }, [])
+
   return <Stack>
-    <Typography variant="h1">윤상지가 세상을 구한다...</Typography>
+    <Typography variant="h1" color={color}>윤상지가 세상을 구한다...</Typography>
   </Stack>
 }
 
