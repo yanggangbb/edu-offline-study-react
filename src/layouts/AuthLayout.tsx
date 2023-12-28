c6efae2032c2134976f739f386b1bae3292c27c6
Ranranruo <tlsalstjr58@gmail.com>
2023-12-28 20:14:29 +0900

    hello

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -136,6 +136,18 @@ const AuthLayout = () => {
                 </ListItem>
               )}
             </NavLink>
+            <NavLink to='/N10407'>
+              {({ isActive }) => (
+                <ListItem disablePadding>
+                  <ListItemButton selected={isActive}>
+                    <ListItemIcon>
+                      <HomeIcon />
+                    </ListItemIcon>
+                    <ListItemText>신민석</ListItemText>
+                  </ListItemButton>
+                </ListItem>
+              )}
+            </NavLink>
           </List>
           <Divider />
           <List subheader={<ListSubheader>게시글</ListSubheader>}>
---
+++ src/pages/N10407.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from '@mui/material';
+
+const N10407 = () => {
+  return (
+    <Stack>
+      <Typography variant='h1'>신민석</Typography>
+    </Stack>
+  );
+};
+
+export default N10407;
